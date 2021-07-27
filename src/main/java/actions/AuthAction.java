49c437ca3be4288afb76f2445448c9e22e0ddb81
Yoshikazu Takahashi <yoshikazu_takahashi@r.recruit.co.jp>
2021-07-27 17:40:29 +0900

    ログイン画面表示アクションの作成

---
+++ src/main/java/actions/AuthAction.java
@@ -0,0 +1,42 @@
+package actions;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import constants.AttributeConst;
+import constants.ForwardConst;
+import services.EmployeeService;
+
+public class AuthAction extends ActionBase {
+
+    private EmployeeService service;
+
+    @Override
+    public void process() throws ServletException, IOException {
+
+        service = new EmployeeService();
+
+        //メソッドを実行
+        invoke();
+
+        service.close();
+    }
+
+    public void showLogin() throws ServletException, IOException {
+
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
+
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
+            putRequestScope(
+                    AttributeConst.FLUSH,
+                    getSessionScope(AttributeConst.FLUSH));
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        forward(ForwardConst.FW_LOGIN);
+    }
+
+
+}
