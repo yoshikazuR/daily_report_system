e57699428ebf8a1c6139313aace60d5b59f16b9c
Yoshikazu Takahashi <yoshikazu_takahashi@r.recruit.co.jp>
2021-07-27 15:30:11 +0900

    EmployeeValidator、EmployeeService作成

---
+++ src/main/java/listeners/PropertiesListener.java
@@ -0,0 +1,62 @@
+package listeners;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class PropertiesListener
+ *
+ */
+@WebListener
+public class PropertiesListener implements ServletContextListener {
+
+    /**
+     * Default constructor.
+     */
+    public PropertiesListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  {
+         // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent arg0)  {
+        ServletContext context = arg0.getServletContext();
+
+        //プロパティファイルを読み込み、アプリケーションスコープに設定する
+        try {
+            InputStream is = PropertiesListener.class.getClassLoader().getResourceAsStream("application.properties");
+
+            Properties properties = new Properties();
+            properties.load(is);
+            is.close();
+
+            Iterator<String> pit = properties.stringPropertyNames().iterator();
+            while (pit.hasNext()) {
+                String pname = pit.next();
+                context.setAttribute(pname, properties.getProperty(pname));
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
