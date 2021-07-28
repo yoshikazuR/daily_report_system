8ddaebfd041275b18a232db33b69b5b082789115
Yoshikazu Takahashi <yoshikazu_takahashi@r.recruit.co.jp>
2021-07-28 17:14:20 +0900

    日報indexアクションの作成

---
+++ src/main/java/actions/ReportAction.java
@@ -0,0 +1,49 @@
+package actions;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import actions.views.ReportView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
+
+public class ReportAction extends ActionBase {
+    private ReportService service;
+
+    @Override
+    public void process() throws ServletException, IOException {
+
+        service = new ReportService();
+
+        //メソッドを実行
+        invoke();
+        service.close();
+    }
+
+    public void index() throws ServletException, IOException {
+
+        int page = getPage();
+        List<ReportView> reports = service.getAllPerPage(page);
+
+        long reportsCount = service.countAll();
+
+        putRequestScope(AttributeConst.REPORTS, reports);
+        putRequestScope(AttributeConst.REP_COUNT, reportsCount);
+        putRequestScope(AttributeConst.PAGE, page);
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
+
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        //一覧画面を表示
+        forward(ForwardConst.FW_REP_INDEX);
+    }
+
+}
