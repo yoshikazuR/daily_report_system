9045e4a4cf440c24a3c8a4cfddb7e0e7ee00ebb9
Yoshikazu Takahashi <yoshikazu_takahashi@r.recruit.co.jp>
2021-07-28 17:39:41 +0900

    日報editアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -22,7 +22,6 @@ public class ReportAction extends ActionBase {
 
         service = new ReportService();
 
-        //メソッドを実行
         invoke();
         service.close();
     }
@@ -45,30 +44,25 @@ public class ReportAction extends ActionBase {
             removeSessionScope(AttributeConst.FLUSH);
         }
 
-        //一覧画面を表示
         forward(ForwardConst.FW_REP_INDEX);
     }
 
     public void entryNew() throws ServletException, IOException {
 
-        putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
 
-        //日報情報の空インスタンスに、日報の日付＝今日の日付を設定する
         ReportView rv = new ReportView();
         rv.setReportDate(LocalDate.now());
-        putRequestScope(AttributeConst.REPORT, rv); //日付のみ設定済みの日報インスタンス
+        putRequestScope(AttributeConst.REPORT, rv);
 
-        //新規登録画面を表示
         forward(ForwardConst.FW_REP_NEW);
 
     }
 
     public void create() throws ServletException, IOException {
 
-        //CSRF対策 tokenのチェック
         if (checkToken()) {
 
-            //日報の日付が入力されていなければ、今日の日付を設定
             LocalDate day = null;
             if (getRequestParam(AttributeConst.REP_DATE) == null
                     || getRequestParam(AttributeConst.REP_DATE).equals("")) {
@@ -77,39 +71,30 @@ public class ReportAction extends ActionBase {
                 day = LocalDate.parse(getRequestParam(AttributeConst.REP_DATE));
             }
 
-            //セッションからログイン中の従業員情報を取得
             EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
 
-            //パラメータの値をもとに日報情報のインスタンスを作成する
             ReportView rv = new ReportView(
                     null,
-                    ev, //ログインしている従業員を、日報作成者として登録する
+                    ev,
                     day,
                     getRequestParam(AttributeConst.REP_TITLE),
                     getRequestParam(AttributeConst.REP_CONTENT),
                     null,
                     null);
 
-            //日報情報登録
             List<String> errors = service.create(rv);
 
             if (errors.size() > 0) {
-                //登録中にエラーがあった場合
 
-                putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
-                putRequestScope(AttributeConst.REPORT, rv);//入力された日報情報
-                putRequestScope(AttributeConst.ERR, errors);//エラーのリスト
+                putRequestScope(AttributeConst.TOKEN, getTokenId());
+                putRequestScope(AttributeConst.REPORT, rv);
+                putRequestScope(AttributeConst.ERR, errors);
 
-                //新規登録画面を再表示
                 forward(ForwardConst.FW_REP_NEW);
 
             } else {
-                //登録中にエラーがなかった場合
-
-                //セッションに登録完了のフラッシュメッセージを設定
                 putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
 
-                //一覧画面にリダイレクト
                 redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
             }
         }
@@ -117,20 +102,37 @@ public class ReportAction extends ActionBase {
 
     public void show() throws ServletException, IOException {
 
-        //idを条件に日報データを取得する
         ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
 
         if (rv == null) {
-            //該当の日報データが存在しない場合はエラー画面を表示
             forward(ForwardConst.FW_ERR_UNKNOWN);
 
         } else {
 
-            putRequestScope(AttributeConst.REPORT, rv); //取得した日報データ
+            putRequestScope(AttributeConst.REPORT, rv);
 
             //詳細画面を表示
             forward(ForwardConst.FW_REP_SHOW);
         }
+    }
+
+    public void edit() throws ServletException, IOException {
+
+        ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+        EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+        if (rv == null || ev.getId() != rv.getEmployee().getId()) {
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+
+        } else {
+
+            putRequestScope(AttributeConst.TOKEN, getTokenId());
+            putRequestScope(AttributeConst.REPORT, rv);
+
+            forward(ForwardConst.FW_REP_EDIT);
+        }
+
     }
 
 }
